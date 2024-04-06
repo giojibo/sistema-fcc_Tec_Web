75c11a85d06dc216200b6c50dd6b20b24df44dd6
Giovani <bonillagiovani6@gmail.com>
2024-04-05 21:31:50 -0600

    Lista admin

--- src/app/screens/admin-screen/admin-screen.component.ts
+++ src/app/screens/admin-screen/admin-screen.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdministradoresService } from 'src/app/services/administradores.service';
+import { FacadeService } from 'src/app/services/facade.service';
 
 @Component({
   selector: 'app-admin-screen',
   templateUrl: './admin-screen.component.html',
   styleUrls: ['./admin-screen.component.scss']
 })
-export class AdminScreenComponent {
+export class AdminScreenComponent implements OnInit{
 
+  public name_user:string = "";
+  public lista_admins:any[]= [];
+
+  constructor(
+    public facadeService: FacadeService,
+    private administradoresService: AdministradoresService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.name_user = this.facadeService.getUserCompleteName();
+  }
+
+  //Funcion para editar
+  public goEditar(idUser: number){
+    this.router.navigate(["registro/"+idUser]);
+  }
+
+  public delete(idUser: number){
+
+  }
 }
